4bd6c508f66167fd17f5616f7f88841b44124171
Vijay Prajapati <127927062+vjy2080@users.noreply.github.com>
2024-08-02 15:31:17 +0530

    add infinite scroll in home page.

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import useAuth from "./components/useAuth";
 import { fetchHomeData } from './components/query';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { DNA } from 'react-loader-spinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Home = () => {
   useAuth();
@@ -12,8 +13,9 @@ const Home = () => {
   const [displayedItems, setDisplayedItems] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalItemsLength, setTotalItemsLength] = useState(0);
-
-  const { data, isFetching, error } = useInfiniteQuery({
+  const [isFetching, setIsFetching] = useState(false)
+  const perPageItems = 8;
+  const { data, error } = useInfiniteQuery({
     queryKey: ['homeData'],
     queryFn: fetchHomeData,
     getNextPageParam: (lastPage) => lastPage.nextPage ?? false,
@@ -23,12 +25,14 @@ const Home = () => {
     if (data) {
       const allItems = data.pages.flatMap((page) => page);
       setTotalItemsLength(allItems.length);
-      setDisplayedItems(allItems.slice(0, currentPage * 8));
+      setDisplayedItems(allItems.slice(0, currentPage * perPageItems));
+      setIsFetching(false);
     }
   }, [data, currentPage]);
 
   const loadMoreItems = () => {
-    setCurrentPage((prevPage) => prevPage + 1);
+    setIsFetching(true);
+    setCurrentPage(currentPage + 1);
   };
 
   if (error) return <div>Error: {error.message}</div>;
@@ -47,38 +51,50 @@ const Home = () => {
 
   return (
     <>
-      <div className="bg-gray-100 grid grid-cols-1 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center content-center">
-        {!isFetching && displayedItems.map((item, index) => (
-          <div
-            onClick={() => setModalItem(item)}
-            key={index}
-            className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white cursor-pointer">
-            <img className="w-full h-48" src={item.imageURL} alt="Sunset in the mountains" />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{item.title}</div>
-              <p className="text-gray-700 text-base">{`${item.description?.slice(0, 30)}...`}</p>
-            </div>
-            <div className="px-6 pt-4 pb-2">
-              {item.tags?.map((tag, tagIndex) => (
-                <span key={tagIndex} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                  {`# ${tag}`}
-                </span>
-              ))}
-            </div>
+      <InfiniteScroll
+        dataLength={totalItemsLength} //This is important field to render the next data
+        next={loadMoreItems}
+        hasMore={currentPage !== totalItemsLength / perPageItems}
+        loader={
+          <div className='flex bg-gray-100  items-center justify-center'>
+            <DNA
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="dna-loading"
+              wrapperStyle={{}}
+              wrapperClass="dna-wrapper"
+            />
           </div>
-        ))}
-      </div>
-
-      {currentPage * 8 < totalItemsLength && (
-        <div className="flex justify-center mt-4">
-          <button
-            onClick={loadMoreItems}
-            className="px-4 py-2 bg-blue-500 text-white rounded-md"
-          >
-            Load More
-          </button>
+        }
+        endMessage={
+          <p className='text-center bg-teal-800 text-white'>
+            <b>Yay! You have seen it all</b>
+          </p>
+        }
+      >
+        <div className="bg-gray-100 grid grid-cols-1 p-20 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center content-center">
+          {!isFetching && displayedItems.map((item, index) => (
+            <div
+              onClick={() => setModalItem(item)}
+              key={index}
+              className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white cursor-pointer">
+              <img className="w-full h-48" src={item.imageURL} alt="Sunset in the mountains" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{item.title}</div>
+                <p className="text-gray-700 text-base">{`${item.description?.slice(0, 30)}...`}</p>
+              </div>
+              <div className="px-6 pt-4 pb-2">
+                {item.tags?.map((tag, tagIndex) => (
+                  <span key={tagIndex} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    {`# ${tag}`}
+                  </span>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
-      )}
+      </InfiniteScroll>
 
       {modalItem && (
         <CardModal
